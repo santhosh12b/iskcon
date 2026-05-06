4a2d2d7ee9457b4565f75a526f410ae798daeb6f
santhosh12b <santhosh12b2020@gmail.com>
2026-05-06 11:12:32 +0530

    fix: change email port to 587 and add tls settings for reliability

--- server/utils/emailSender.js
+++ server/utils/emailSender.js
@@ -3,13 +3,17 @@ const nodemailer = require('nodemailer');
 const sendTicketEmail = async (userEmail, userName, eventName, pdfBuffer, booking = {}) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // true for 465, false for other ports
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+        tls: {
+            rejectUnauthorized: false // Helps with some hosting provider blocks
+        }
     });
+
 
 
     const mailOptions = {
